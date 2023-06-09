d33afeddfd222686e9e5ac9c993eea88e8153206
maliksaepul <malik@inspigo.id>
2023-06-09 10:20:26 +0700

    feat : add lint-staged

--- src/views/HomeView/HomeView.tsx
+++ src/views/HomeView/HomeView.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const HomeView = () => {
-    return <div>HomeView</div>
+    return (
+        <React.Fragment>
+            <section>Hero for input search</section>
+            <section>list of images</section>
+        </React.Fragment>
+    )
 }
 
 export default HomeView
