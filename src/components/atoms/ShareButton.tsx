3557ed0fc3c1562372e22488b792b0c08fe7fbab
maliksaepul <malik@inspigo.id>
2023-06-10 20:41:38 +0700

    feat : modal photo

--- src/components/atoms/ShareButton.tsx
+++ src/components/atoms/ShareButton.tsx
@@ -1,6 +1,7 @@
 import { Popover } from '@headlessui/react'
 import React from 'react'
 import SVG from 'react-inlinesvg'
+import { twMerge } from 'tailwind-merge'
 
 const ListSosmed = [
     {
@@ -17,7 +18,22 @@ const ListSosmed = [
     },
 ]
 
-const ShareButton = () => {
+export enum PopoverPosition {
+    TOP_RIGHT = 'top-right',
+    BOTTOM_LEFT = 'bottom-left',
+    BOTTOM_RIGHT = 'bottom-right',
+    TOP_LEFT = 'top-left',
+}
+
+type ShareButtonProps = {
+    popover?:
+        | PopoverPosition.BOTTOM_LEFT
+        | PopoverPosition.BOTTOM_RIGHT
+        | PopoverPosition.TOP_LEFT
+        | PopoverPosition.TOP_RIGHT
+}
+
+const ShareButton: React.FC<ShareButtonProps> = ({ popover = 'top-right' }) => {
     return (
         <Popover className={'relative'}>
             <Popover.Button className="flex items-center gap-1 rounded-sm border border-solid">
@@ -25,7 +41,11 @@ const ShareButton = () => {
                     <SVG src="/assets/icons/share.svg" className="h-4 w-4" />
                 </div>
             </Popover.Button>
-            <Popover.Panel className="absolute bottom-[120%] right-0  flex flex-col gap-2 rounded-sm bg-white p-4 text-black shadow-sm">
+            <Popover.Panel
+                className={twMerge(
+                    'absolute bottom-[120%] right-0 flex h-fit  w-fit flex-col gap-2 rounded-sm bg-white p-4 text-black shadow-sm',
+                    getVariantClass(popover)
+                )}>
                 {ListSosmed.map((item, index) => (
                     <div key={index} className="flex cursor-pointer select-none items-center gap-2">
                         <div className="flex h-8 w-8 items-center justify-center rounded-full border border-solid">
@@ -37,6 +57,21 @@ const ShareButton = () => {
             </Popover.Panel>
         </Popover>
     )
+}
+
+const getVariantClass = (variant: string) => {
+    switch (variant) {
+        case PopoverPosition.TOP_RIGHT:
+            return 'bottom-[120%] right-0'
+        case PopoverPosition.BOTTOM_LEFT:
+            return 'top-[120%] left-0'
+        case PopoverPosition.BOTTOM_RIGHT:
+            return 'top-[120%] right-0'
+        case PopoverPosition.TOP_LEFT:
+            return 'bottom-[120%] left-0'
+        default:
+            return 'bottom-[120%] right-0'
+    }
 }
 
 export default ShareButton
--- src/components/molecules/PhotoModal.tsx
+++ src/components/molecules/PhotoModal.tsx
@@ -2,6 +2,9 @@ import { Photo } from '@/lib/types'
 import { Dialog } from '@headlessui/react'
 import Image from 'next/image'
 import React from 'react'
+import Avatar from '../atoms/Avatar'
+import FavoriteButton from '../atoms/FavoriteButton'
+import ShareButton, { PopoverPosition } from '../atoms/ShareButton'
 
 type ModalPhotoProps = {
     isOpen: boolean
@@ -11,27 +14,49 @@ type ModalPhotoProps = {
 
 const ModalPhoto: React.FC<ModalPhotoProps> = ({ isOpen, handleCloseModal, data }) => {
     return (
-        <Dialog
-            className={'relative z-30 cursor-zoom-out'}
-            open={isOpen}
-            onClose={handleCloseModal}>
-            <Dialog.Backdrop className="fixed inset-0 bg-black opacity-30" />
-            <div className="fixed inset-0 flex items-center justify-center p-4">
-                <Dialog.Panel className="mx-auto max-w-sm rounded bg-white">
-                    {data && (
-                        <React.Fragment>
-                            <Dialog.Title>{data.description}</Dialog.Title>
-                            <Image
-                                src={data.urls.full}
-                                width={data.width}
-                                height={data.height}
-                                alt={data.alt_description}
-                                blurDataURL={data.blur_hash}
-                                onClick={handleCloseModal}
-                            />
-                        </React.Fragment>
-                    )}
-                </Dialog.Panel>
+        <Dialog className={'relative z-50 '} open={isOpen} onClose={handleCloseModal}>
+            <div className="fixed inset-0 cursor-zoom-out bg-black/30" aria-hidden="true" />
+            <div className="fixed inset-0 overflow-y-auto">
+                <div className="flex min-h-full items-center justify-center p-4">
+                    <Dialog.Panel className="relative mx-auto max-w-screen-init rounded bg-white p-5 sm:p-10 md:p-20 lg:p-40">
+                        <div
+                            className="absolute right-0 top-0 flex h-10 w-10 cursor-pointer select-none items-center justify-center rounded-sm border border-solid text-lg font-bold text-slate-700"
+                            onClick={handleCloseModal}>
+                            X
+                        </div>
+                        {data && (
+                            <div className="flex flex-col gap-4">
+                                <Dialog.Title className={'flex flex-col gap-4'}>
+                                    <div className="flex items-center gap-2 text-black">
+                                        <Avatar
+                                            src={data.user.profile_image.medium}
+                                            alt={data.user.name}
+                                        />
+                                        <p className="text-sm font-semibold sm:text-lg">
+                                            {data.user.name}
+                                        </p>
+                                    </div>
+                                    <p className="text-sm font-semibold sm:text-lg">
+                                        {data.description}
+                                    </p>
+                                    <div className="flex gap-2">
+                                        <FavoriteButton />
+                                        <ShareButton popover={PopoverPosition.BOTTOM_LEFT} />
+                                    </div>
+                                </Dialog.Title>
+                                <Image
+                                    src={data.urls.full}
+                                    width={data.width}
+                                    height={data.height}
+                                    alt={data.alt_description}
+                                    blurDataURL={data.blur_hash}
+                                    className="cursor-zoom-out"
+                                    onClick={handleCloseModal}
+                                />
+                            </div>
+                        )}
+                    </Dialog.Panel>
+                </div>
             </div>
         </Dialog>
     )
